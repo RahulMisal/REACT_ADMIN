6961cdf42b92d8e4a329791b61b8d1feb7cce00b
Rahul Misal <86588411+RahulMisal@users.noreply.github.com>
2025-02-20 14:49:53 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var conn = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"react-admin"
+    "host":"buwhivz519kpkjyfmtxe-mysql.services.clever-cloud.com",
+    "user":"ukmmiwq2vzqa5snz",
+    "password":"yNG3AVxfY5fVZDZhox4n",
+    "database":"buwhivz519kpkjyfmtxe"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
